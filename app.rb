1524ca544a945f8b8efde4551fb0783ca5863349
Sam White <sam.white745@gmail.com>
2023-04-18 14:47:40 +0100

    started html

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'space'
 
 class Application < Sinatra::Base
   configure :development do
@@ -8,5 +9,26 @@ class Application < Sinatra::Base
 
   get '/' do
     return erb(:index)
+  end
+
+  get '/spaces/new' do
+    
+    return erb(:list_space_new)
+  end
+
+  post '/spaces' do
+    space = Space.new
+    space.name=params['name']
+    space.description=params['description']
+    space.price_per_night=params['price']
+    start_date = Date.parse(params['start_date'])
+    end_date = Date.parse(params['start_date'])
+    space.available_dates=(start_date..end_date).to_a
+    space.owner_id = 1
+
+    repo = SpacesRepository.new
+    repo.create(space)
+    
+    return erb(:space_posted)
   end
 end
