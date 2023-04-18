0bc84633030033e2957220000a24083fcccfd602
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-04-18 10:56:09 +0100

    Write first test for login view and add DROP TABLE if exists to create_tables.sql

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -17,7 +17,6 @@ describe Application do
   # one test suite for each set of related features),
   # you can duplicate this test file to create a new one.
 
-
   context 'GET /' do
     it 'should get the homepage' do
       response = get('/')
@@ -25,4 +24,24 @@ describe Application do
       expect(response.status).to eq(200)
     end
   end
+
+  context 'GET /login' do
+    context "when the user is logged in" do
+      it "redirects to '/'" do
+        # TODO
+      end
+    end
+    context "when the user isn't logged in" do
+      it "displays a login form" do
+        response = get('/login')
+        expect(response.status).to eq 200
+        expect(response.body).to include('<form method="POST" action="/login_attempt">')
+        expect(response.body).to include('<label for="email">Email Address:</label>')
+        expect(response.body).to include('<input type="text" name="email" />')
+        expect(response.body).to include('<label for="password">Password:</label>')
+        expect(response.body).to include('<input type="password" name="password" />')
+      end
+    end
+  end
+
 end
