2fe9d4fc2da18716dab142d52e803b1e8cc0550c
Sam White <sam.white745@gmail.com>
2023-04-19 17:04:17 +0100

    moved space repo helper

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -9,5 +9,29 @@ class Helper
     return request
   end
 
+  def self.convert_to_date_objects(dates_string)
+    # SQL query of the "available_dates" col returns a single string formatted as: "{YYYY-MM-DD,YYY-MM-DD}"
+    date_array = dates_string[1..-2].split(',')
+
+    return date_array.map { |date| Date.parse(date) }
+  end
+
+  def self.convert_date_objects_to_string(dates_array)
+    available_dates_string = dates_array.map { |date| date.to_s }.join(',')
+    available_dates_string.prepend('{').concat('}')
+  end
+
+  def self.convert_to_space(record)
+    space = Space.new
+    space.id = record['id'].to_i
+    space.name = record['name']
+    space.description = record['description']
+    space.price_per_night = record['price_per_night'].to_f.round(2)
+    space.owner_id = record['owner_id'].to_i
+    space.available_dates = Helper.convert_to_date_objects(record['available_dates'])
+
+    return space
+  end
+
 end
 
